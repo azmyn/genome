c37ffbe7a41c10e255269a98f4b2024be2f8c1da
A <a.08095@gmail.com>
2020-01-23 23:37:04 +0900

    Delete count_1Mb.sh

--- count_1Mb.sh
+++
@@ -1,6 +0,0 @@
-CANCER_TYPE=$1
-
-Rscript count_mut_100Mb.R /Users/azumi/Genome/mc3/mc3.v0.2.8.PUBLIC.maf.gz /Users/azumi/Genome/${CANCER_TYPE}_tcga/data_bcr_clinical_data_patient.txt ${CANCER_TYPE}
-
-# Rscript PCA_plot.R lusc.mut_count.txt /Users/azumi/Genome/lusc_tcga/data_bcr_clinical_data_patient.txt
-# Rscript count_mut.R /Users/azumi/Genome/mc3/mc3.v0.2.8.PUBLIC.maf.gz /Users/azumi/Genome/lusc_tcga/data_bcr_clinical_data_patient.txt > lusc.mut_count.txt
